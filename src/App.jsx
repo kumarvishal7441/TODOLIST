9158f4054e0f6bada62692d80db36ad2450f89d2
kumarvishal7441 <kumarvishal7441@gmail.com>
2026-02-01 19:37:58 +0530

    header and todolist componet created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from './components/Header'
+import ToDoList from './components/ToDoList'
 
 function App() {
   return (
     <div>
       <Header />
+      <ToDoList/>
     </div>
   )
 }
